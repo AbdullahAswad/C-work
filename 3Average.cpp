6ee4c05f4ae83249eeb3cf899b9f3bd1543fc7f6
AbdullahAswad <113939739+AbdullahAswad@users.noreply.github.com>
2023-01-14 23:14:30 -0500

    Create 3Average.cpp

---
+++ 3Average.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float arr[3];
+    float ave = 0;
+    
+    cout << "Input 3 numbers, average will be given" << endl;
+    cin >> arr[0];
+    cin >> arr[1];
+    cin >> arr[2];
+    
+    ave = (arr[0] + arr[1] + arr[2]) / 3;
+    
+    cout << "The average is: " << ave << endl;
+    return 0;
+}
