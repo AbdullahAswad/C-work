8643f50a17b38e2fe932146a1ef2bdabf8002e6f
AbdullahAswad <113939739+AbdullahAswad@users.noreply.github.com>
2023-01-14 23:15:02 -0500

    Create reverse.cpp

---
+++ reverse.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num, num2;
+    int sum = 0;
+    int a, b, c, d;
+    
+    cout << "Input 2 4 digit numbers" << endl;
+    cin >> num;
+    cin >> num2;
+    
+    a = num % 10;
+    b = num / 10 % 10;
+    c = num / 100 % 10;
+    d = num / 1000;
+    num = (a * 1000) + (b * 100) + (c * 10) + d;
+    
+    a = num2 % 10;
+    b = num2 / 10 % 10;
+    c = num2 / 100 % 10;
+    d = num2 / 1000;
+    num2 = (a * 1000) + (b * 100) + (c * 10) + d;
+    
+    sum = num + num2;
+    
+    cout << "First number reversed: " << num << endl;
+    cout << "Second number reversed: " << num2 << endl;
+    cout << "Total sum: " << sum << endl;
+
+  
+    return 0;
+}
